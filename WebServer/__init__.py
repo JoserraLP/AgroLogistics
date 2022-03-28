66aede6d827957d7c8780cd05f64badd3f335b19
joserralp <jolozanop@alumnos.unex.es>
2022-03-28 10:28:48 +0200

    Removed unused libraries

--- WebServer/__init__.py
+++ WebServer/__init__.py
@@ -1,30 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_mqtt import Mqtt
 from flask_login import LoginManager
 from flask_user import UserManager
 
-from geopy.geocoders import Nominatim
-
 from .config import default
-
-# init geolocator
-geolocator = Nominatim(user_agent=__name__)
 
 # init SQLAlchemy
 db = SQLAlchemy()
 
-# init MQTT
-mqtt = Mqtt()
-
-@mqtt.on_connect()
-def handle_connect(client, userdata, flags, rc):
-    ''' Handle connection to MQTT broker. '''
-    print("Connected to MQTT")
-
 def create_app():
     ''' Create a Flask app, configure it. register some project blueprints as 'auth' or 'main', 
-        initialize related services as MQTT or SQLAlchemy (with data insertion) and the flask login manager.
+        initialize related services as SQLAlchemy (with data insertion) and the flask login manager.
 
         Returns:
             app (object): Configured Flask app
@@ -46,17 +32,15 @@ def create_app():
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    from .stock import stock as stock_blueprint
-    app.register_blueprint(stock_blueprint)
+    from .statistics import statistics as statistics_blueprint
+    app.register_blueprint(statistics_blueprint)
 
     from .schedule import schedule as schedule_blueprint
     app.register_blueprint(schedule_blueprint)
 
-    # Init the MQTT service
-    #mqtt.init_app(app)
+    from .transaction import transaction as transaction_blueprint
+    app.register_blueprint(transaction_blueprint)
 
-    # Subscribe to CEP observations
-    mqtt.subscribe("Observations")
 
     # Import models to create the tables
     from .models import User, Role
@@ -74,16 +58,6 @@ def create_app():
         if not User.query.limit(1).all() and not Role.query.limit(1).all():
             from .utils.insert_data_to_db import insert_user_data
             insert_user_data(db)
-
-    @mqtt.on_message()
-    def on_message(client, userdata, message):
-        if (message.topic == "Observations"):
-            with app.app_context():
-                obs = str(message.payload.decode("utf-8")).split(',')
-
-                from .utils.insert_data_to_db import insert_observation_data        
-                
-                insert_observation_data(db, obs=obs)
 
     # Create the LoginManager
     login_manager = LoginManager()
